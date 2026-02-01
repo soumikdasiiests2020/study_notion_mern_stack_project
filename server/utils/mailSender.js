90265a71843fa963cab8a18427862282b3eb93f2
SOUMIK DAS <121462445+soumikdasiiests2020@users.noreply.github.com>
2026-02-01 21:21:05 +0530

    Update mailSender.js

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,4 +1,4 @@
-/*const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
     try{
@@ -27,44 +27,9 @@ const mailSender = async (email, title, body) => {
 
 
 module.exports = mailSender;
-*/
 
-const nodemailer = require("nodemailer");
 
-const mailSender = async (email, title, body) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",          // IMPORTANT
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.MAIL_USER, // your gmail
-        pass: process.env.MAIL_PASS, // 16-char app password
-      },
-      tls: {
-        rejectUnauthorized: false,  // IMPORTANT for Render
-      },
-      connectionTimeout: 10000, // 10 sec
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
-    });
 
-    const info = await transporter.sendMail({
-      from: `"StudyNotion" <${process.env.MAIL_USER}>`,
-      to: email,
-      subject: title,
-      html: body,
-    });
-
-    console.log("Mail sent:", info.response);
-    return info;
-  } catch (error) {
-    console.error("Mail error:", error);
-  }
-};
-
-module.exports = mailSender;
 
 
 
